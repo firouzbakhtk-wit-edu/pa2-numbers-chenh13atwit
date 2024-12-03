5c359e6fda742ef5f6484b37662a2cff4882fb72
haili <haili@10.220.93.195>
2024-12-03 09:45:55 -0500

    pa2

--- src/edu/wit/cs/comp1000/PA2a.java
+++ src/edu/wit/cs/comp1000/PA2a.java
@@ -1,11 +1,53 @@
 package edu.wit.cs.comp1000;
 
+import java.util.Scanner;
+
 // TODO: document this class
 public class PA2a {
 
 	// TODO: document this function
 	public static void main(String[] args) {
-		// TODO: write your code here
+		Scanner scanner = new Scanner(System.in);
+		
+		int sumPositive = 0, sumNonPositive = 0, sumAll = 0;
+		int countPositive = 0, countNonPositive = 0;
+		
+		System.out.print("Enter five whole numbers: ");
+		
+		for (int i = 0; i < 5; i++) {
+			int num = scanner.nextInt();
+			sumAll += num;
+			
+			if (num > 0) {
+				sumPositive += num;
+				countPositive++;
+			} else {
+				sumNonPositive += num;
+				countNonPositive++;
+			}
+		}
+		
+		double averagePositive = (countPositive > 0) ? (double) sumPositive / countPositive : 0.0;
+		double averageNonPositive = (countNonPositive > 0) ? (double) sumNonPositive / countNonPositive : 0.0;
+		double averageAll = (double) sumAll / 5;
+		
+		 System.out.printf("The sum of the %d positive number%s: %d%n",
+                 countPositive, countPositive == 1 ? "" : "s", sumPositive);
+		
+         System.out.printf("The sum of the %d non-positive number%s: %d%n",
+                 countNonPositive, countNonPositive == 1 ? "" : "s", sumNonPositive);
+         
+         System.out.printf("The sum of the 5 numbers: %d%n", sumAll);
+         
+         System.out.printf("The average of the %d positive number%s: %.2f%n",
+                 countPositive, countPositive == 1 ? "" : "s", averagePositive);
+        
+         System.out.printf("The average of the %d non-positive number%s: %.2f%n",
+                 countNonPositive, countNonPositive == 1 ? "" : "s", averageNonPositive);
+         
+         System.out.printf("The average of the 5 numbers: %.2f%n", averageAll);
+		
+		scanner.close();
 	}
 
 }
--- src/edu/wit/cs/comp1000/PA2b.java
+++ src/edu/wit/cs/comp1000/PA2b.java
@@ -1,11 +1,37 @@
 package edu.wit.cs.comp1000;
 
+import java.util.Scanner;
+
 // TODO: document this class
 public class PA2b {
 
 	// TODO: document this function
 	public static void main(String[] args) {
-		// TODO: write your code here
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Enter a b c: ");
+		
+		double a = scanner.nextDouble();
+		double b = scanner.nextDouble();
+		double c = scanner.nextDouble();
+		
+		double discriminant = b * b - 4 * a * c;
+		
+		if (a == 0) {
+            System.out.println("This is not a quadratic equation.");
+        } else if (discriminant < 0) {
+            System.out.println("Roots: imaginary");
+        } else if (discriminant == 0) {
+            double root = -b / (2 * a);
+            System.out.printf("Root: %.2f%n", root);
+        } else {
+            double root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+            double root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+            System.out.printf("Roots: %.2f, %.2f%n", root2, root1);
+        }
+		scanner.close();
+
+		
 	}
 
 }
